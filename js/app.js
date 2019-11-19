fdd43c205d5058b17da99089301e2dd7002ddaa6
Mattäus S. Isenberg <mattaus@gmx.com>
2019-11-19 10:04:16 -0800

    clear commented out code

--- js/app.js
+++ js/app.js
@@ -56,27 +56,24 @@ function Producto(producto) {
       this.clickCtr++;
   }
 
-  var firstThree = randomTres();
+  
 
-  var leftProduct = new Producto(arrayOfAll[firstThree[0]]);
+  var leftProduct;
   var leftProductImage = document.getElementById('leftProduct_img');
-  leftProductImage.setAttribute('src', leftProduct.imgUrl);
   var leftProductNameElement = document.getElementById('leftProduct_heading');
-  leftProductNameElement.textContent = leftProduct.name;
+  
   leftProductImage.addEventListener('click', clickHandler);
 
-  var centerProduct = new Producto(arrayOfAll[firstThree[1]]);
+  var centerProduct;
   var centerProductImage = document.getElementById('centerProduct_img');
-  centerProductImage.setAttribute('src', centerProduct.imgUrl);
   var centerProductNameElement = document.getElementById('centerProduct_heading');
-  centerProductNameElement.textContent = centerProduct.name;
+  
   centerProductImage.addEventListener('click', clickHandler);
 
-  var rightProduct = new Producto(arrayOfAll[firstThree[2]]);
-  var rightProductImage = document.getElementById('rightProduct_img');
-  rightProductImage.src = rightProduct.imgUrl;
-  var rightProductNameElement = document.getElementById('rightProduct_heading');
-  rightProductNameElement.textContent = rightProduct.name;
+  rightProduct;
+  rightProductImage = document.getElementById('rightProduct_img');
+  rightProductNameElement = document.getElementById('rightProduct_heading');
+  
   rightProductImage.addEventListener('click', clickHandler);
 
 
@@ -99,21 +96,15 @@ function drawIt(){
     var new3 = randomTres();
 
     leftProduct = new Producto(arrayOfAll[new3[0]]);
-    leftProductImage = document.getElementById('leftProduct_img');
     leftProductImage.setAttribute('src', leftProduct.imgUrl);
-    leftProductNameElement = document.getElementById('leftProduct_heading');
     leftProductNameElement.textContent = leftProduct.name;
   
     centerProduct = new Producto(arrayOfAll[new3[1]]);
-    centerProductImage = document.getElementById('centerProduct_img');
     centerProductImage.setAttribute('src', centerProduct.imgUrl);
-    centerProductNameElement = document.getElementById('centerProduct_heading');
     centerProductNameElement.textContent = centerProduct.name;
 
     rightProduct = new Producto(arrayOfAll[new3[2]]);
-    rightProductImage = document.getElementById('rightProduct_img');
-    rightProductImage.src = rightProduct.imgUrl;
-    rightProductNameElement = document.getElementById('rightProduct_heading');
+    rightProductImage.setAttribute('src', rightProduct.imgUrl);
     rightProductNameElement.textContent = rightProduct.name;
 
 }
@@ -121,18 +112,20 @@ function drawIt(){
 
 
 function clickHandler(event) {
+    
     console.log(`${leftProduct.name}:${dictionaryOfClicks[leftProduct.name]}-1`);
     console.log(`${centerProduct.name}:${dictionaryOfClicks[centerProduct.name]}-2`);
     console.log(`${rightProduct.name}:${dictionaryOfClicks[rightProduct.name]}-3`);
+
     var id = event.target.id;
   
     if(id == 'leftProduct_img') {
         dictionaryOfClicks[leftProduct.name] += 1;
         console.log(`${leftProduct.name}:${dictionaryOfClicks[leftProduct.name]}`);
-    } else if('centerProduct_img') {
+    }else if(id == 'centerProduct_img') {
         dictionaryOfClicks[centerProduct.name] += 1;
         console.log(`${centerProduct.name}:${dictionaryOfClicks[centerProduct.name]}`);
-    }else if('rightProduct_img'){
+    }else if(id == 'rightProduct_img'){
         dictionaryOfClicks[rightProduct.name] += 1;
         console.log(`${rightProduct.name}:${dictionaryOfClicks[rightProduct.name]}`);
     }
@@ -146,3 +139,5 @@ function clickHandler(event) {
 
     drawIt();
 }
+
+drawIt();
