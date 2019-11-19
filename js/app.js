5c67e9fdbb5f4af6f011266530f002c891836035
Mattäus S. Isenberg <mattaus@gmx.com>
2019-11-19 11:11:07 -0800

    clear commented out code

--- js/app.js
+++ js/app.js
@@ -41,6 +41,28 @@ var dictionaryOfClicks = {
     'water-can':0,
     'wine-glass':0}
 
+var dictionaryOfViews = {
+
+        'bag':0,
+        'banana':0,
+        'bathroom':0,
+        'boots':0,
+        'breakfast':0,
+        'bubblegum':0,
+        'chair':0,
+        'cthulhu':0,
+        'dog-duck':0,
+        'dragon':0,
+        'pen':0,
+        'pet-sweep':0,
+        'scissors':0,
+        'shark':0,
+        'tauntaun':0,
+        'unicorn':0,
+        'water-can':0,
+        'wine-glass':0}
+
+
 function Producto(producto) {
 
     this.name = producto;
@@ -98,14 +120,17 @@ function drawIt(){
     leftProduct = new Producto(arrayOfAll[new3[0]]);
     leftProductImage.setAttribute('src', leftProduct.imgUrl);
     leftProductNameElement.textContent = leftProduct.name;
+    dictionaryOfViews[leftProduct.name] += 1;
   
     centerProduct = new Producto(arrayOfAll[new3[1]]);
     centerProductImage.setAttribute('src', centerProduct.imgUrl);
     centerProductNameElement.textContent = centerProduct.name;
+    dictionaryOfViews[centerProduct.name] += 1;
 
     rightProduct = new Producto(arrayOfAll[new3[2]]);
     rightProductImage.setAttribute('src', rightProduct.imgUrl);
     rightProductNameElement.textContent = rightProduct.name;
+    dictionaryOfViews[rightProduct.name] += 1;
 
 }
 
@@ -113,31 +138,37 @@ function drawIt(){
 
 function clickHandler(event) {
     
-    console.log(`${leftProduct.name}:${dictionaryOfClicks[leftProduct.name]}-1`);
-    console.log(`${centerProduct.name}:${dictionaryOfClicks[centerProduct.name]}-2`);
-    console.log(`${rightProduct.name}:${dictionaryOfClicks[rightProduct.name]}-3`);
-
     var id = event.target.id;
   
     if(id == 'leftProduct_img') {
         dictionaryOfClicks[leftProduct.name] += 1;
-        console.log(`${leftProduct.name}:${dictionaryOfClicks[leftProduct.name]}`);
+        leftProduct.clickCountPlus();
     }else if(id == 'centerProduct_img') {
         dictionaryOfClicks[centerProduct.name] += 1;
-        console.log(`${centerProduct.name}:${dictionaryOfClicks[centerProduct.name]}`);
+        centerProduct.clickCountPlus();
     }else if(id == 'rightProduct_img'){
         dictionaryOfClicks[rightProduct.name] += 1;
-        console.log(`${rightProduct.name}:${dictionaryOfClicks[rightProduct.name]}`);
+        rightProduct.clickCountPlus();
     }
     
+    // console.log(`${leftProduct.name}:${dictionaryOfClicks[leftProduct.name]} clicks`);
+    // console.log(`${leftProduct.name}:${dictionaryOfViews[leftProduct.name]} views`);
+    // console.log(`${centerProduct.name}:${dictionaryOfClicks[centerProduct.name]} clicks`);
+    // console.log(`${leftProduct.name}:${dictionaryOfViews[centerProduct.name]} views`);
+    // console.log(`${rightProduct.name}:${dictionaryOfClicks[rightProduct.name]} clicks`);
+    // console.log(`${leftProduct.name}:${dictionaryOfViews[rightProduct.name]} views`);
 
-    if(Producto.voteCount == Producto.maxVote) {
-        leftImageElem.removeEventListener('click', clickHandler);
-        centerImageElem.removeEventListener('click', clickHandler);
-        rightImageElem.removeEventListener('click', clickHandler);
-      }
-
-    drawIt();
+    for (key in dictionaryOfClicks){
+        if (dictionaryOfClicks[key] < 25){
+            drawIt();
+        }else if(dictionaryOfClicks[key] === 25){
+            window.alert(`${dictionaryOfClicks[key]} voted 25 times!`);
+            leftImageElem.removeEventListener('click', clickHandler);
+            centerImageElem.removeEventListener('click', clickHandler);
+            rightImageElem.removeEventListener('click', clickHandler);
+            break;
+        }
+    }
 }
 
 drawIt();
